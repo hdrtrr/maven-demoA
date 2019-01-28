9652765bf91b8c9387bbf931b517479ebd129e3b
hdrtrr <gzrjjmh@qq.com>
2019-01-28 14:27:59 +0800

    first commit

---
+++ src/main/java/try_test/controller/HelloController.java
@@ -0,0 +1,33 @@
+package try_test.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class HelloController {
+
+    @RequestMapping("testA")
+    String testA() {
+        int a = 1;
+        int b = 2;
+        int c = a*b;
+
+        return "testA";
+
+    }
+
+
+
+    @RequestMapping("hello")
+    String hello() {
+        return "Hello World123!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class,args);
+    }
+}
