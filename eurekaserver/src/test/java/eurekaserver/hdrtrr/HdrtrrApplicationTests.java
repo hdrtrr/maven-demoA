cffce1aaefdabdc4acfdda45b8e395b3de6d44d5
hdrtrr <gzrjjmh@qq.com>
2019-03-06 11:00:21 +0800

    eureka server

---
+++ eurekaserver/src/test/java/eurekaserver/hdrtrr/HdrtrrApplicationTests.java
@@ -0,0 +1,16 @@
+package eurekaserver.hdrtrr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HdrtrrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
