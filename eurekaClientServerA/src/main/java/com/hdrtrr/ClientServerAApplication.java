7c866c926fce9e731aaa533a925a61bd61c358fc
hdrtrr <gzrjjmh@qq.com>
2019-04-08 14:12:22 +0800

    123

---
+++ eurekaClientServerA/src/main/java/com/hdrtrr/ClientServerAApplication.java
@@ -0,0 +1,13 @@
+package com.hdrtrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerAApplication.class, args);
+    }
+
+}
